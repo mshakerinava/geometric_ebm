e5313751af898451c0120192dddee1476e8e053d
Mehran Shakerinava <mehranshakerinava@gmail.com>
2022-12-17 03:04:48 -0500

    sync

--- sbatch_all.sh
+++ sbatch_all.sh
@@ -1,15 +1,15 @@
-sbatch batch_long.sh --manifold euclidean --dataset 8gaussians
-sbatch batch_long.sh --manifold sphere --dataset 8gaussians
-sbatch batch_long.sh --manifold torus --dataset 8gaussians
+sbatch batch_long.sh --cpu --manifold euclidean --dataset 8gaussians
+sbatch batch_long.sh --cpu --manifold sphere --dataset 8gaussians
+sbatch batch_long.sh --cpu --manifold torus --dataset 8gaussians
 
-sbatch batch_long.sh --manifold euclidean --dataset 2spirals
-sbatch batch_long.sh --manifold sphere --dataset 2spirals
-sbatch batch_long.sh --manifold torus --dataset 2spirals
+sbatch batch_long.sh --cpu --manifold euclidean --dataset 2spirals
+sbatch batch_long.sh --cpu --manifold sphere --dataset 2spirals
+sbatch batch_long.sh --cpu --manifold torus --dataset 2spirals
 
-sbatch batch_long.sh --manifold euclidean --dataset checkerboard
-sbatch batch_long.sh --manifold sphere --dataset checkerboard
-sbatch batch_long.sh --manifold torus --dataset checkerboard
+sbatch batch_long.sh --cpu --manifold euclidean --dataset checkerboard
+sbatch batch_long.sh --cpu --manifold sphere --dataset checkerboard
+sbatch batch_long.sh --cpu --manifold torus --dataset checkerboard
 
-sbatch batch_long.sh --manifold euclidean --dataset rings
-sbatch batch_long.sh --manifold sphere --dataset rings
-sbatch batch_long.sh --manifold torus --dataset rings
+sbatch batch_long.sh --cpu --manifold euclidean --dataset rings
+sbatch batch_long.sh --cpu --manifold sphere --dataset rings
+sbatch batch_long.sh --cpu --manifold torus --dataset rings
